7b10c30a66c94872b3954671231a178a236a7a4d
zeiglers <32206428+zeiglers@users.noreply.github.com>
2017-12-05 21:16:47 -0800

    Delete headerTemplate.js

--- tenative_putTogether/public/postTemplates/headerTemplate.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['header'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<header>\r\n    <div class=\"logoContainer\">\r\n        <img src=\"./logo.jpg\" alt=\"Our Logo\" />\r\n    </div>\r\n</header>";
-},"useData":true});
-})();
