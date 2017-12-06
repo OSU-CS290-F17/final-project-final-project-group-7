3f8c733c7d4f5f0ae5be63e2baa63aab3c13d219
zeiglers <32206428+zeiglers@users.noreply.github.com>
2017-12-05 21:13:30 -0800

    Delete build.sh

--- tenative_putTogether/build.sh
+++
@@ -1,4 +0,0 @@
-echo "i will now run the build script."
-
-handlebars views/templates/imgPost.handlebars views/templates/textPost.handlebars views/templates/listPost.handlebars -f public/postTemplates/postTemplates.js
-handlebars views/partials/imgModal.handlebars views/partials/textModal.handlebars views/partials/textModal.handlebars -f public/postTemplates/modaltemplates.js
